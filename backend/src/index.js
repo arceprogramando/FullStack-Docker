93547814d3647767212d382b031b232ec13343bb
Felipe Arce <arceprogramando@gmail.com>
2024-08-31 11:49:10 -0300

    feat: change origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   }),
